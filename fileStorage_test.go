f8220636f4ef9e9947927910d055d11c8a065f99
Ivan Gerasin <ivan@gerasin.dev>
2023-02-27 20:16:37 +0100

    actual tests

---
+++ fileStorage_test.go
@@ -0,0 +1,51 @@
+package mtdCore
+
+import "os"
+
+type MockedFileSystem struct {
+}
+type FileMock struct {
+	close error
+	seek  struct {
+		ret int64;
+		err error
+	}
+	stat struct {
+		finfo os.FileInfo;
+		error
+	}
+	read struct {
+		n   int;
+		err error
+	}
+	write struct {
+		n   int;
+		err error
+	}
+}
+
+func (fileMock FileMock) Close() error {
+	return fileMock.close
+}
+
+func (fileMock FileMock) Seek(_ int64, _ int) (ret int64, err error) {
+	return fileMock.seek.ret, fileMock.seek.err
+}
+
+func (fileMock FileMock) Stat() (os.FileInfo, error) {
+	return fileMock.stat.finfo, fileMock.stat.error
+}
+
+func (fileMock FileMock) Read(_ []byte) (n int, err error) {
+	return fileMock.read.n, fileMock.read.err
+}
+
+func (fileMock FileMock) Write(_ []byte) (n int, err error) {
+	return fileMock.write.n, fileMock.write.err
+}
+
+var fileMockPrototype = FileMock{}
+
+func (fs MockedFileSystem) OpenFile(name string, flag int, perm int) (File, error) {
+	return fileMockPrototype, nil
+}
---
+++ mtdCore_test.go
@@ -0,0 +1,105 @@
+package mtdCore
+
+import (
+	"testing"
+)
+
+type MockedStorage struct {
+}
+
+const testItemId = 99
+
+func (fs MockedStorage) ReadTodoList() (error, *ToDoGlobal) {
+	return nil, &ToDoGlobal{
+		ToDoItem{
+			Id:       testItemId,
+			Summary:  "Test item",
+			Done:     false,
+			Priority: 9,
+		},
+	}
+}
+
+func (fs MockedStorage) SaveToDoList(lst *ToDoGlobal) error {
+	saveToDoListCalls = lst
+	return nil
+}
+
+var storageMock = &MockedStorage{}
+var original TodoListStorage
+
+var saveToDoListCalls *ToDoGlobal
+
+func setup() {
+	original = Storage
+	saveToDoListCalls = nil
+	Storage = storageMock
+}
+
+func tearDown() {
+	Storage = original
+}
+
+func TestList(t *testing.T) {
+	setup()
+
+	t.Run("Should return list of items", func(t *testing.T) {
+		result := List() // Expected list of 1 element
+		if len(*result) != 1 {
+			t.Error("Incorrect list size")
+		}
+		if (*result)[0].Id != testItemId {
+			t.Error("Expecting different id value")
+		}
+		if (*result)[0].Summary != "Test item" {
+			t.Error("Expecting different Summary value")
+		}
+		if (*result)[0].Done != false {
+			t.Error("Expecting Done to be false")
+		}
+		if (*result)[0].Priority != 9 {
+			t.Error("Expecting different Summary value")
+		}
+	})
+
+	tearDown()
+}
+
+func TestAddItem(t *testing.T) {
+	setup()
+
+	t.Run("Should call SaveToDoList with new item", func(t *testing.T) {
+		AddItem("New text", 1)
+		if saveToDoListCalls == nil {
+			t.Error("Should not be nil")
+		}
+		if len(*saveToDoListCalls) != 2 {
+			t.Error("Should add new element to list")
+		}
+		if (*saveToDoListCalls)[1].Id != testItemId+1 {
+			t.Error("Should assign Id that greater than existing by 1")
+		}
+		if (*saveToDoListCalls)[1].Summary != "New text" {
+			t.Error("Should save summary text")
+		}
+		if (*saveToDoListCalls)[1].Priority != 1 {
+			t.Error("Should save priority value")
+		}
+		if (*saveToDoListCalls)[1].Done {
+			t.Error("Should not set Done flag")
+		}
+	})
+
+	tearDown()
+}
+
+func TestDone(t *testing.T) {
+	setup()
+	t.Run("Should mark given test item as done", func(t *testing.T) {
+		Done(testItemId)
+		if (*saveToDoListCalls)[0].Done != true {
+			t.Error("Should set Done flag")
+		}
+	})
+	tearDown()
+}
